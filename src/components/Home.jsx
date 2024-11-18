ee0c14deefdda1407454fb9db6fc186589d00c45
Hema Bindra <hbindra11@yahoo.com>
2024-11-18 19:41:12 +0100

    cards with  mock blogpost title, content and image and button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,41 @@
 const Home = () => {
-    return(<></>)
-}
+  const mockPosts = [
+    {
+      title: "Maltese summer",
+      content: "Beautiful day at the beach... ",
+      cover: "holiday picture url",
+      date: "2021-08-15",
+    },
+    {
+      title: "Austrian Alps in summer",
+      content: "Beautiful day in the mountains...",
+      cover: "Cabel car picture url",
+      date: "2022-09-15",
+    },
+  ];
+
+  return (
+    <>
+      {mockPosts.map((post) => (
+        <div
+          className="card card-side bg-base-100 shadow-2xl my-2 "
+          key={crypto.randomUUID()}
+        >
+         
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+              alt="picture of New York city"
+            />    
+          </figure> <div className="card-body">
+          <h2 className="card-title">{post.title}</h2><p>{post.content}</p> 
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Details</button>
+    </div></div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ const Navbar = () => {
     <>
       <nav className=" bg-green-900 text-white p-4">
         <div className=" flex justify-between container mx-auto">
-          <h2>TravelBlogPosts</h2>
+        <Link to="/" className="text-white text-lg font-bold">
+        TravelBlogPosts
+        </Link>
+          
           <div className="flex space-x-4">
             <Link to={"/"}>Home</Link>
 
